d6102a52fa88fb3d6c8e0d38c01cd0601162a5b0
Cyrus Bowman <cyrusbow@gmail.com>
2020-02-01 17:26:07 -0500

    - Now only populate login if running as dev

--- src/overmind/login/state.js
+++ src/overmind/login/state.js
@@ -1,6 +1,7 @@
+const IS_DEV = process.env.NODE_ENV == 'development';
 
 export default {
   name: '',
-  email: '',
-  password: ''
+  email: IS_DEV ? 'michael@smithfield.com' : '',
+  password: IS_DEV ? 'password' : ''
 }
